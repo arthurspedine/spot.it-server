3c7f1d725db24e1e76afd9a143a95f5f322afaf6
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2024-10-07 23:20:47 -0300

    chore: configures basic fastify plugins

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import z from 'zod'
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
+  CORS_ORIGINS: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,41 @@
 import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: env.CORS_ORIGINS.split(','),
+  credentials: true,
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'AutoInsight',
+      description: 'Especificações da API para o back-end da AutoInsight.',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 async function main() {
   await app.listen({
